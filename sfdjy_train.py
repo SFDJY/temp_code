ad3e5b207249c7fcee3efb8e49b179c6d10023fb
SFDJY <71162125+SFDJY@users.noreply.github.com>
2020-09-14 09:16:16 +0800

    Update sfdjy_train.py

--- sfdjy_train.py
+++ sfdjy_train.py
@@ -4,7 +4,7 @@ import torch
 from torch import nn, optim
 from torch.autograd import Variable
 
-from sfdjy_dataset import sfdjy_data_loader, sfdjy_test_data_loader
+from sfdjy_dataset import sfdjy_data_loader
 from SFDJY_BiSeNet import BiSeNet
 from sfdjy_utils import Evaluator
 from sfdjy_dataset import n_class
@@ -36,37 +36,37 @@ def sfdjy_train(model, epochs=100):
 
             acc = (pred == label).float().mean()
 
-            if i % 10000 == 0:
+            if i % 100 == 0:
                 print('=============================== ' + str(i) + ' ===================================')
                 print("epoch: {}/{}, loss: {:.6f}, running_acc: {:.6f}"
                       .format(epoch + 1, epochs, loss.item(), acc.item()))
 
-        model.eval()
-        accuracy = 0.0
-        number_train_data = 0
-        sfdjy_miou = Evaluator(num_class=n_class)
-        for i, data in enumerate(sfdjy_test_data_loader):
-            image, label = data
-            image = image.permute(0, 3, 1, 2)
-            label = label.long()
-            image = image.cuda()
-            label = label.cuda()
-            image = Variable(image)
-            label = Variable(label)
+#         model.eval()
+#         accuracy = 0.0
+#         number_train_data = 0
+#         sfdjy_miou = Evaluator(num_class=n_class)
+#         for i, data in enumerate(sfdjy_test_data_loader):
+#             image, label = data
+#             image = image.permute(0, 3, 1, 2)
+#             label = label.long()
+#             image = image.cuda()
+#             label = label.cuda()
+#             image = Variable(image)
+#             label = Variable(label)
 
-            out = model(image)
+#             out = model(image)
 
-            _, pred = torch.max(out, dim=1)
+#             _, pred = torch.max(out, dim=1)
 
-            accuracy += (pred == label).float().mean()
-            print('acc = ', (pred == label).float().mean())
+#             accuracy += (pred == label).float().mean()
+#             print('acc = ', (pred == label).float().mean())
 
-            sfdjy_miou.add_batch(label.cpu(), pred.cpu())
-            print('miou = ', sfdjy_miou.Mean_Intersection_over_Union())
-            sfdjy_miou.reset()
-            number_train_data = i
+#             sfdjy_miou.add_batch(label.cpu(), pred.cpu())
+#             print('miou = ', sfdjy_miou.Mean_Intersection_over_Union())
+#             sfdjy_miou.reset()
+#             number_train_data = i
 
-        print('accuracy = ', accuracy / number_train_data)
+#         print('accuracy = ', accuracy / number_train_data)
 
         torch.save(model.state_dict(), './sfdjy_bisenet.pth')
 
